e503a5563a7efb518a7747ee891a6b1c52b9f9e5
SantinoFajardo <santinofajardo02@gmail.com>
2022-06-28 23:22:57 -0300

    28/06-23:22

---
+++ Formulario/formulario.js
@@ -0,0 +1,13 @@
+// Menu desplegable
+addEventListener('DOMContentLoaded' , () =>{
+    const btn__menu = document.querySelector('.btn__menu')
+    if(btn__menu){
+        btn__menu.addEventListener('click', () =>{
+            const menu__items = document.querySelector('.menu__items')
+            menu__items.classList.toggle('show')
+        })
+    }
+})
+
+// .......................................................................... //
+
